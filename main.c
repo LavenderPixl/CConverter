4278f94b6cf3d170a13869394f1a79729d0dee0d
LavenderPixl <annajacobsen1999@hotmail.com>
2025-01-17 11:44:17 +0100

    First commit.
    Decimal to Binary and Hexadecimal works currently.

---
+++ main.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <string.h>
+
+int get_user_decimal();
+
+void decimal_to_binary(int);
+
+void decimal_to_hex(int);
+
+void reverse_array(char []);
+
+int main(void) {
+    const int user_input = get_user_decimal();
+    decimal_to_binary(user_input);
+    decimal_to_hex(user_input);
+    return 0;
+}
+
+int get_user_decimal() {
+    printf("Enter your decimal number: ");
+    // Get user input.
+    int user_input = 0;
+    scanf("%d", &user_input);
+    return user_input;
+}
+
+void decimal_to_binary(int decimal_number) {
+    int quotient;
+    int temp_array[40];
+    int i = 0;
+
+
+    do {
+        quotient = decimal_number / 2;
+        const int remainder = decimal_number % 2;
+        decimal_number = quotient;
+        temp_array[i] = remainder;
+        i++;
+    } while (quotient != 0);
+
+    printf("Binary: ");
+    for (int d = 0; d < i; d++) {
+        printf("%d", temp_array[d]);
+    }
+    printf("\n");
+}
+
+void decimal_to_hex(int decimal_number) {
+    char temp_array[64] = {0};
+    int quotient = 1;
+    int i = 0;
+
+    do {
+        const int remainder = decimal_number % 16;
+        if (remainder < 10) {
+            temp_array[i] = (48 + remainder);
+        } else {
+            temp_array[i] = (55 + remainder);
+        }
+
+        quotient = decimal_number / 16;
+        decimal_number = quotient;
+
+        i++;
+    } while (quotient != 0);
+
+
+    reverse_array(temp_array);
+    printf("Hexadecimal: 0x");
+    for (int d = 0; d < i; d++) {
+        printf("%c", temp_array[d]);
+    }
+    printf("\n");
+}
+
+void binary_to_decimal(int binary_number) {
+}
+
+void reverse_array(char arr[]) {
+    int size = strlen(arr);
+
+    int a = 0;
+    int b = size - 1;
+
+    while (b - a > 0) {
+        arr[a] = arr[a] ^ arr[b];
+        arr[b] = arr[a] ^ arr[b];
+        arr[a] = arr[a] ^ arr[b];
+
+        a++;
+        b--;
+    }
+}
